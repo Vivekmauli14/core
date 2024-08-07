b04f282c1801c331cc1ce17b872069fb912361aa
Franco Fichtner <franco@opnsense.org>
2024-08-07 15:14:44 +0200

    interfaces: move PPP-related IPv6 behaviour into linkup #7446

--- src/opnsense/scripts/interfaces/ppp-linkup.sh
+++ src/opnsense/scripts/interfaces/ppp-linkup.sh
@@ -21,11 +21,15 @@ fi
 if [ "${2}" = "inet" ]; then
 	/usr/local/sbin/ifctl -i ${1} -4nd ${DNS1} ${DNS2}
 	/usr/local/sbin/ifctl -i ${1} -4rd ${ROUTER}
+	/usr/local/opnsense/scripts/interfaces/ppp-ipv6.php ${1} 4
 	/usr/local/sbin/configctl -d interface newip ${1} force
 elif [ "${2}" = "inet6" ]; then
 	/usr/local/sbin/ifctl -i ${1} -6nd ${DNS1} ${DNS2}
 	/usr/local/sbin/ifctl -i ${1} -6rd ${ROUTER}
-	/usr/local/sbin/configctl -d interface newipv6 ${1} force
+	if ! /usr/local/opnsense/scripts/interfaces/ppp-ipv6.php ${1} 6; then
+		# trigger event here since no higher layer IPv6 will trigger it
+		/usr/local/sbin/configctl -d interface newipv6 ${1} force
+	fi
 fi
 
 touch /tmp/${1}_uptime
